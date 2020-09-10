603901502b7dc0e690cddd0003da6838799611d7
Jayant Choudhary <jayant2204@gmail.com>
2020-09-10 18:17:35 +0530

    Revert "Revert "Service Caller added""
    This reverts commit 633e119e19d0ff13a02103c918809719b5b87fe4.

--- lib/app/modules/SplashScreen/splashScreen.dart
+++ lib/app/modules/SplashScreen/splashScreen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.center,
+      child: CircularProgressIndicator(),
+    );
   }
 }
