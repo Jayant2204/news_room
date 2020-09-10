02b6484bd2f6162164af51eb5ca74e9c9c42c197
Jayant Choudhary <jayant2204@gmail.com>
2020-09-10 18:17:50 +0530

    Revert "Revert "Rough Design""
    This reverts commit 308c2d6584301b65e171dc199ece2f8762bd2e28.

--- lib/app/modules/news/newsPage.dart
+++ lib/app/modules/news/newsPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:get/get.dart';
 import 'package:news_room/app/modules/news/newsController.dart';
+import 'package:news_room/app/modules/news/widget/newsCard.dart';
 
 class NewsShowCaseScreen extends StatelessWidget {
   final NewsController _newsController = Get.put(NewsController());
@@ -25,6 +27,14 @@ class NewsShowCaseScreen extends StatelessWidget {
         icon: Icon(CupertinoIcons.share_up),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+      body: Container(
+          child: Swiper(
+        itemCount: _newsController.obj.length,
+        itemBuilder: (context, index) {
+          return NewsCardWidget(article: _newsController.obj[index]);
+        },
+      )),
     );
   }
 }
