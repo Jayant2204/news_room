633e119e19d0ff13a02103c918809719b5b87fe4
Jayant Choudhary <jayant2204@gmail.com>
2020-09-10 18:16:55 +0530

    Revert "Service Caller added"
    This reverts commit de9398d8ed35c990a6745f648a3047e73e32ae60.

--- lib/app/data/provider/constants.dart
+++
@@ -1,9 +0,0 @@
-class Constant {
-  static const String newsApiKey = 'b9cbfe99c70c4a6587bb5c0628b4a30f';
-  static const String baseUrl = 'https://newsapi.org/v2/';
-  static const String topHeadLine = '/top-headlines';
-  static const String worldNews = 'world';
-  static const String indiaNews = 'india';
-  static const String sportsNews = 'sports';
-  static const String techNews = 'technology';
-}
--- lib/app/modules/SplashScreen/splashScreen.dart
+++ lib/app/modules/SplashScreen/splashScreen.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: CircularProgressIndicator(),
-    );
+    return Container();
   }
 }
